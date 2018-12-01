009d836c854344f0f2a0b1c14f62ea6b27893f00
Jarkko Saarinen <jsaarinen92@gmail.com>
2018-12-01 15:48:49 +0200

    const to function

--- src/listitems/index.js
+++ src/listitems/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ListItemsScreen from "./ListItemsScreen";
 import { localDataLoader, networkDataLoader } from "./DataLoaders";
 
-const wrapListItemsWithDataLoader = (dataLoader) => {
+function wrapListItemsWithDataLoader(dataLoader) {
   return class extends React.Component {
     render() {
       return <ListItemsScreen loadData={dataLoader} {...this.props} />
