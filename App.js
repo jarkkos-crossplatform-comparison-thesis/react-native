db18ae2be5843ccf89ec17519360d7280cceba47
Jarkko Saarinen <jsaarinen92@gmail.com>
2018-12-01 15:58:56 +0200

    Add header titles to screens

--- App.js
+++ App.js
@@ -20,13 +20,48 @@ class NotImplemented extends React.Component {
 //https://github.com/react-navigation/react-navigation/issues/1254#issuecomment-346319921
 const App = createStackNavigator(
   {
-    Landing: { screen: SelectTestScreen },
-    ButtonLatency: { screen: ButtonLatencyScreen },
-    LocalListView: { screen: LocalListItemsScreen },
-    NetworkListView: { screen: NetworkListItemsScreen },
-    HeavyComputation: { screen: HeavyComputationScreen },
-    VibrationLatency: { screen: VibrationLatency },
-    ThirdPartyNotices: { screen: NotImplemented }
+    Landing: {
+      screen: SelectTestScreen,
+      navigationOptions: ({ navigation }) => ({
+        title: "Thesis benchmarking"
+      })
+    },
+    ButtonLatency: {
+      screen: ButtonLatencyScreen,
+      navigationOptions: ({ navigation }) => ({
+        title: "Button latency"
+      })
+    },
+    LocalListView: {
+      screen: LocalListItemsScreen,
+      navigationOptions: ({ navigation }) => ({
+        title: "Local listview"
+      })
+    },
+    NetworkListView: {
+      screen: NetworkListItemsScreen,
+      navigationOptions: ({ navigation }) => ({
+        title: "Network listview"
+      })
+    },
+    HeavyComputation: {
+      screen: HeavyComputationScreen,
+      navigationOptions: ({ navigation }) => ({
+        title: "Heavy computation"
+      })
+    },
+    VibrationLatency: {
+      screen: VibrationLatency,
+      navigationOptions: ({ navigation }) => ({
+        title: "Vibration latency"
+      })
+    },
+    ThirdPartyNotices: {
+      screen: NotImplemented,
+      navigationOptions: ({ navigation }) => ({
+        title: "Third party notices"
+      })
+    }
   }, {
     transitionConfig: () => ({
       transitionSpec: {
@@ -35,20 +70,20 @@ const App = createStackNavigator(
         easing: Easing.step0,
       },
       screenInterpolator: sceneProps => {
-         const { layout, position, scene } = sceneProps;
-         const { index } = scene;
-         const width = layout.initWidth;
-         const translateX = position.interpolate({
-           inputRange: [index - 1, index, index + 1],
-           outputRange: [width, 0, 0],
-         });
-         if (index <= 1){
-             return {};
-         }
-  
-         return {transform: [{ translateX }] };
-       }
-  })
+        const { layout, position, scene } = sceneProps;
+        const { index } = scene;
+        const width = layout.initWidth;
+        const translateX = position.interpolate({
+          inputRange: [index - 1, index, index + 1],
+          outputRange: [width, 0, 0],
+        });
+        if (index <= 1) {
+          return {};
+        }
+
+        return { transform: [{ translateX }] };
+      }
+    })
   });
 
 export default App;
